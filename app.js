62e1c81653e73ba8787f74785d5834f173dc704c
Zhakhin Arden <zhakhinarden@gmail.com>
2019-03-31 22:30:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -21,11 +21,22 @@ app.get("/", function(req, res) {
 
 app.get("/search", function(req, res) {
   // var str  = new RegExp(req.query.word, 'z');
-  var str = req.query.word;
-  Song.find({ lastword: {$regex: str+'$'}}).then(songs => {
-    res.render("index", { songs: songs });
-  });
+  // var str = '/' + req.query.word + '/';
+  const word = req.query.word;
+  const str = '(' + req.query.word.split('').map((s, i) => {
+    if (i < word.length - 1) {
+      return s + word.slice(-word.length + i + 1);
+    }
+  }).join('|').slice(0, -1) + ')';
+  // for (i=0; i<str.lenght; i++){
+  //   str = str.substring(i)
+    Song.find({ lastword: {$regex: str+'$'}}).then(songs => {
+      res.render("index", { songs });
+    });
+  // }
+  
 });
+
 
 app.get("/create", function(req, res) {
   res.render("create");
