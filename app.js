797a0c6610fbc08667415728e7c493c43298a42a
Gorbunova <alinagorbunova98@mail.ru>
2019-04-28 23:37:13 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const Song = require("./models/song");
+const upload = require("express-fileupload");
 
 var app = express();
+
+const { dbImport, dbExport } = require("./dbLibs");
+app.use(upload());
+app.use(cookieParser());
 
 app.use(express.static("views"));
 
@@ -33,11 +39,60 @@ app.get("/api/statistic", function(req, res) {
       .join("|")
       .slice(0, -1) +
     ")";
-  // for (i=0; i<str.lenght; i++){
-  //   str = str.substring(i)
-  Song.find({ lastword: { $regex: str + "$" } }).then(songs => {
+
+  // Song.find({ lastword: { $regex: str + "$" } }).then(songs => {
+  //   res.send(songs);
+  // });
+  Song.aggregate([
+    { $match: { lastword: { $regex: str + "$" } } },
+    { $project: { _id: 0, gr_song: "$title" } },
+    { $group: { _id: { gr_song: "$gr_song" }, hits: { $sum: 1 } } }
+  ]).then(songs => {
     res.send(songs);
   });
+});
+
+app.get("/api/statistic777", function(req, res) {
+  const word = req.query.word;
+  const str =
+    "(" +
+    req.query.word
+      .split("")
+      .map((s, i) => {
+        if (i < word.length - 1) {
+          return s + word.slice(-word.length + i + 1);
+        }
+      })
+      .join("|")
+      .slice(0, -1) +
+    ")";
+
+  Song.aggregate([
+    { $match: { lastword: { $regex: str + "$" } } },
+    { $project: { _id: 0, gr_word: "$lastword" } },
+    { $group: { _id: { gr_word: "$gr_word" }, whits: { $sum: 1 } } }
+  ]).then(songs => {
+    res.send(songs);
+  });
+});
+
+app.post("/import", (req, res) => {
+  const file = req.files.json;
+  const doc = JSON.parse(file.data.toString("utf-8"));
+
+  dbImport(doc).then(() => {
+    res.sendStatus(200);
+  });
+});
+
+app.get("/export", (req, res) => {
+  const reqSession = req.cookies.session;
+
+  if (reqSession && sessions[reqSession]) {
+    dbExport(sessions[reqSession]).then(json => {
+      res.send(json);
+    });
+  }
 });
 
 app.get("/search", function(req, res) {
